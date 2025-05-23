38cc3412da22da28cd5daca01c835b8eaa6ffe31
Albeehu <yujhen880104.gmail.com>
2025-05-23 15:00:32 +0800

    本地資料庫

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
+import json
+import os
 import requests
+
+def load_products():
+    with open('fake_store_data.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    response = requests.get("https://fakestoreapi.com/products")
-    print(response.json())
-    products = response.json()
+    products = load_products()
     return render_template("index.html", products=products)
 
 if __name__ == "__main__":
     app.run(debug=True)
+# def index():
+#     response = requests.get("https://fakestoreapi.com/products")
+#     print(response.json())
+#     products = response.json()
+#     return render_template("index.html", products=products)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
