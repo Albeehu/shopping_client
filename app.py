485cda6fc61a6fffcdd6b09b41c9ee97250421e2
Albeehu <yujhen880104.gmail.com>
2025-05-23 16:58:27 +0800

    新增banner

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for  # 添加 url_for 导入
 import json
 import os
 import requests
@@ -11,16 +11,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    banner_image = url_for('static', filename='images/banner.jpg')
     products = load_products()
-    return render_template("index.html", products=products)
+    return render_template('index.html', 
+                         banner_image=banner_image,
+                         products=products)
 
 if __name__ == "__main__":
     app.run(debug=True)
-# def index():
-#     response = requests.get("https://fakestoreapi.com/products")
-#     print(response.json())
-#     products = response.json()
-#     return render_template("index.html", products=products)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
